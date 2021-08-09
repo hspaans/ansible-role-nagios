f4fa0539c7e1cdb291fd808662404e1bfb291838
Hans Spaans <hans@dailystuff.nl>
2021-08-09 23:54:25 +0200

    Initial update

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,7 @@ import pytest
 
 @pytest.mark.parametrize("pkg", ["nagios4"])
 def test_pkg_installed(host, pkg):
+    """Test if package installed."""
     package = host.package(pkg)
 
     assert package.is_installed
